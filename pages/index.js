b18d9ec071bfcfa5beab35f14af2b288b98b467d
MarcelaMemoria <marcela.amemoria@gmail.com>
2025-03-19 19:19:19 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Olá Mundo!! Essa é a minha página inicial JavaScript</h1>
+    return <h1>Olá Mundo!! Essa é a minha página inicial do Curso.Dev!</h1>
 }
 
 export default Home;
